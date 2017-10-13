ad85200a3fbf8a0408ebaa53d58ce4c87ac4c3c4
Michael Melnikov <32643279+phpindex@users.noreply.github.com>
2017-10-13 19:29:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 https://www.youtube.com/watch?v=b1lVAFJoMmA&index=1&list=PL1B5MFiEjZtpf4Ll3-Jgq4R8hVQfahkvV
 http://4programmer.com/1201/PHP-%26-MySQL-OOP-MVC-awebarts-CMS
+https://githubuniverse.com/?utm_source=github&utm_medium=banner&utm_campaign=ww-dotcom-dash-universe-20170717
