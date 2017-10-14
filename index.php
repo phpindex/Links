f7debc91e0c573f5360537fdd76416ef8fe6dbfc
Michael Melnikov <32643279+phpindex@users.noreply.github.com>
2017-10-14 15:49:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 https://www.youtube.com/watch?v=b1lVAFJoMmA&index=1&list=PL1B5MFiEjZtpf4Ll3-Jgq4R8hVQfahkvV
 http://4programmer.com/1201/PHP-%26-MySQL-OOP-MVC-awebarts-CMS
 https://githubuniverse.com/?utm_source=github&utm_medium=banner&utm_campaign=ww-dotcom-dash-universe-20170717
+https://www.codecourse.com/lessons/google-maps-mongodb-search
+https://www.codecourse.com/lessons/build-a-shopping-cart
+https://www.codecourse.com/lessons/laravel-private-messages-dashboard
+https://www.codecourse.com/lessons/laravel-avatar-uploads
+https://www.codecourse.com/lessons/laravel-chatroom
+https://www.codecourse.com/lessons/git-and-github
